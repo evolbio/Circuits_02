adb237c222b7fe6d3e45f295179858c4763850cd
Steve Frank <saf@stevefrank.org>
2024-05-31 07:40:21 -0700

    Made figure.

--- src/Anomaly.jl
+++ src/Anomaly.jl
@@ -2,38 +2,45 @@ module Anomaly
 using JumpProcesses, Plots, Random, DifferentialEquations
 export driver
 
-function driver()
-	sol = dynamics()
-	pl = plot(layout=(2,1),size=(1000,800))
-	plot!(sol.t,[sol[1,:],sol[2,:]], subplot=1,lw=[2 3],
+function driver(; base_in=100, m2_frac=0.1, r_scale=25, rstate = nothing)
+	if rstate === nothing
+		rstate = copy(Random.default_rng())
+		println(rstate)
+	end
+	copy!(Random.default_rng(), rstate)
+	m2 = m2_frac*base_in
+	sol = dynamics(base_in)
+	pl = plot(layout=(2,1),size=(750,600))
+	plot!(sol.t,[sol[1,:],sol[2,:]], subplot=1,lw=[1.5 2.25],
 		label=["input value" "internal average"])
 	plot!(sol.t,[sol[4,:],
-		[receptor0(sol[2,i],sol[1,i],100,10,2,1) .* 25 for i in 1:length(sol[1,:])]],
-		subplot=2,lw=[3 2.5],xlabel="Time", label=["anomaly jumps" "receptor response"])
+		[receptor0(sol[2,i],sol[1,i],base_in,m2,2,1) .* r_scale for i in 1:length(sol[1,:])]],
+		subplot=2,lw=[2.25 1.8],xlabel="Time", label=["anomaly jumps" "receptor response"])
 	display(pl)
 	return sol, pl
 end
 
-function dynamics()
+function dynamics(base_in; tmax=20)
 	default(; lw=2)
-	jump_amplitude = 0.2  	# Amplitude of the jumps
+	jump_size = 0.05  		# Amplitude of the jumps
 	rate(u,p,t) = 0.2		# Rate of the Poisson process
+	sigma = 0.02			# diffusion coeff for multiplicative noise
 	
 	function f!(du, u, p, t)
-		du[1] = 0.0002(100 - u[1])
-		du[2] = 5.0(u[1] - u[2])
-		du[3] = 2.0(1 - u[3])
+		du[1] = 0.0002(base_in - u[1])
+		du[2] = 10.0(u[1] - u[2])
+		du[3] = 2.0(1 - u[3])		# placeholder for a dynamics, not used
 		du[4] = -60*u[4]
 	end
 	function g!(du, u, p, t) 
-		du[1] = 0.02*u[1]
+		du[1] = sigma*u[1]			# multiplicative noise
 		du[2:4] .= zeros(3)
 	end
-	affect!(integrator) = (integrator.u[1] *= jmp=1+sign(randn())*0.05;
+	affect!(integrator) = (integrator.u[1] *= jmp=1+sign(randn())*jump_size;
 		integrator.u[4] = (jmp>1) ? 1 : -1)
 
-	u0 = [100.0,100.0,1.0,0.0]
-	tspan = (0.0, 20.0)
+	u0 = [base_in,base_in,1.0,0.0]
+	tspan = (0.0, tmax)
 	
 	prob = SDEProblem(f!, g!, u0, tspan, dt=1e-13)
 	const_jump = ConstantRateJump(rate, affect!)
--- src/Run.jl
+++ src/Run.jl
@@ -1,4 +1,9 @@
-using Anomaly, Plots
+using Anomaly, Plots, Random
 
 sol,pl = driver();
+savefig(pl,"/Users/steve/Desktop/anomaly.pdf")
+
+# for publication figure
+rstate = Xoshiro(0xa242084f5b199bea, 0x53a1df5ce28618fd, 0x89250c8d5127cb4e, 0x6b5f53c46218257b, 0xc5ed5ca8a7cca17e);
+sol,pl = driver(;rstate=rstate);
 savefig(pl,"/Users/steve/Desktop/anomaly.pdf")
